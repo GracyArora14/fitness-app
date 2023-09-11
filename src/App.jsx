60c4b641b21578e354737a1c1be51983f75c9699
Gracy Arora <gr4059@srmist.edu.in>
2023-09-11 19:25:46 +0530

    commit1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+import Landing from './Components/Landing';
+import './index.css'
+
+
+function App() {
+  return (
+    <div className='App' > 
+   <Landing/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Landing.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import pic3 from '../images/pic3.png'
+import "./Landing.css"
+
+function Landing() {
+  return (
+    <div className='landing-container'>
+     
+      <div className='text'>
+      <h1>Transform your Life,</h1>
+        <p className='typewriter' >One Text at a Time</p>
+        <div className="button-container">
+            <button className="button">Let's get Started !!</button>
+          </div>
+        </div>
+      
+     
+    </div>
+   
+  )
+}
+
+export default Landing
